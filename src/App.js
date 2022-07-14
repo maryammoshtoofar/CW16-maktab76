2d3147182493d6b9fef2eda4b7c4b807cbc52c68
raha <slrrraha@gmail.com>
2022-07-14 14:13:42 +0430

    first-commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import Rect from 'Rect';
+import TestComp from './TestComp';
+
+class App {
+  constructor() {
+    this.rect = new Rect();
+  }
+
+  render() {
+    return(
+      <>
+      </>
+    )
+  }
+}
+
+export default App;
