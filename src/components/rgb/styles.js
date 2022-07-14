fc2d88f9cfe61f707835a3a5ba128a62a6512bdf
marziehmokhtari <marziehmokhtari67@gmail.com>
2022-07-14 17:36:24 +0430

    complete q2

--- src/components/rgb/styles.js
+++ src/components/rgb/styles.js
@@ -9,6 +9,14 @@ justify-content: center;
 export const ColorDiv = styled.div`
 
 `
-export const Text = styled.button`
+export const Text = styled.p`
+color: ${({color})=>color}
+`
+export const Button=styled.button`
+border-radius:5px;
+border:1px solid black;
+padding:5px 10px;
+`
+export const Content= styled.div`
 color: ${({color})=>color}
 `
