fa6025649d84a9adff38429ddc9ae29663b26e58
raha <slrrraha@gmail.com>
2022-07-14 14:43:41 +0430

    add component folder

---
+++ src/components/colors/index.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import randomColor from 'randomcolor';
+
+class Colors extends Component{
+    state = {
+        colors: [],
+    }
+    render() {
+      return(
+        <>
+  
+        </>
+      )
+    }
+  }
+  
+  export default Colors;
